1e78813685b52a31d1548ba13bdc75bdb5e8362f
Oscar Bäck <oscar.back96@outlook.com>
2023-12-02 15:52:33 +0100

    Day2 part 1 complete, bug in part 2

---
+++ AOC_2023/Challenges/Day2/CubeConundrum.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Challenges.Day2 {
+    public class CubeConundrum {
+        public static int SolveGame(string data, bool part_two) {
+            if(data == "") return 0;
+            string[] data_arr = data.Split(": ");
+            if (part_two) {
+                return PowerOfSmallest(data_arr[1]);
+            } else {
+                bool isValidGame = IsValidGame(data_arr[1]);
+                int roundNum = ParseGameNumber(data_arr[0]);
+                if (!isValidGame) Console.WriteLine("invalid game data: " + data);
+                return isValidGame ? roundNum : 0;
+            }
+        }
+
+        
+
+        private static int ParseGameNumber(string raw_game_round_data) {
+            return int.Parse(raw_game_round_data.Split(" ")[1]);
+        }
+
+        //Game rules: 12 red, 13 green, 14 blue
+        private static bool IsValidGame(string game_data) {
+
+            string[] rounds = game_data.Split("; ");
+            foreach (string round in rounds) {
+                string[] cubes = round.Split(", ");
+                foreach (string cube in cubes) {
+                    string[] cube_values = cube.Split(" ");
+                   if ((cube_values[1] == "blue" && int.Parse(cube_values[0]) > 14) ||
+                        (cube_values[1] == "green" && int.Parse(cube_values[0]) > 13) ||
+                        (cube_values[1] == "red" && int.Parse(cube_values[0]) > 12))
+                            return false;
+                }
+            }
+
+
+            return true;
+        }
+
+        private static int PowerOfSmallest(string game_data) {
+            IDictionary<string, int> game_summary = new Dictionary<string, int>();
+
+            string[] rounds = game_data.Split("; ");
+            foreach (string round in rounds) {
+                string[] cubes = round.Split(", ");
+                foreach (string cube in cubes) {
+                    string[] cube_values = cube.Split(" ");
+                    if (game_summary.ContainsKey(cube_values[1]) && game_summary[cube_values[1]] < int.Parse(cube_values[0])) {
+                        Console.WriteLine("Adding smallest to " + cube_values[1] + " previous val: " + game_summary[cube_values[1]]);
+                        game_summary[cube_values[1]] = int.Parse(cube_values[0]);
+                    } else game_summary[cube_values[1]] = int.Parse(cube_values[0]);
+                }
+            }
+
+
+            return game_summary["blue"] * game_summary["green"] * game_summary["red"];
+        }
+
+
+        public static int SolvePossibleGamesCubeValue(string game_data, bool part_two) {
+            string[] games = game_data.Split("\n");
+            int sum = 0;
+            foreach (string game in games) {
+
+                sum += SolveGame(game, part_two);
+            }
+            return sum;
+        }
+        
+    }
+}
---
+++ AOC_2023/Challenges_test/Day2/CubeConundrumTest.cs
@@ -0,0 +1,62 @@
+﻿using Challenges.Day2;
+using NUnit.Framework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Challenges_test.Day2 {
+    public class CubeConundrumTest {
+
+        [SetUp] 
+        public void SetUp() { 
+        
+        }
+
+        [Test]
+        public void ValidateSuccessfullGame() {
+            string test_data = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+            int res = CubeConundrum.SolveGame(test_data, false);
+            Assert.AreEqual(1, res);
+        }
+
+        [Test]
+        public void ValidateUnsuccessfullGame() {
+            string test_data = "Game 1: 3 blue, 34 red; 1 red, 2 green, 6 blue; 2 green";
+            int res = CubeConundrum.SolveGame(test_data, false);
+            Assert.AreEqual(0, res);
+        }
+
+        [Test]
+        public void ValidateGameWithMaxValue() {
+            string test_data = "Game 1: 1 blue, 1 red, 1 green; 11 red, 12 green, 13 blue";
+            int res = CubeConundrum.SolveGame(test_data, false);
+            Assert.AreEqual(1, res);
+        }
+
+        [Test]
+        public void SolveCubeValueWithTestData() {
+            string data = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+            int res = CubeConundrum.SolvePossibleGamesCubeValue(data, false);
+            Assert.AreEqual(8, res);
+        }
+
+        [Test]
+        public void SolveCubeValueTest() {
+            string data = File.ReadAllText("Day2/input.txt");
+
+            int res = CubeConundrum.SolvePossibleGamesCubeValue(data, false);
+
+            Assert.AreEqual(1931, res);
+        }
+
+        [Test]
+        public void SolvePowerOfSmallestCubeWithTestData() {
+            string data = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+            int res = CubeConundrum.SolvePossibleGamesCubeValue(data, true);
+            Assert.AreEqual(2286, res);
+        }
+    }
+}
