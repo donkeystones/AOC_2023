30bd06033baff55af51d8340ec3119ce2cc087cb
Oscar Bäck <oscar.back96@outlook.com>
2023-12-03 19:44:06 +0100

    Day 3 part One done
    Super freaking wierd that a number on the far right counts a schematic when there is no symbol.

---
+++ AOC_2023/Challenges/Day3/GearRatios.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges.Day3 {
+    public class GearRatios {
+        public static int FindSumOfSchematicNumbers(char[,] schematic) {
+            int sum = 0;
+
+            for(int y = 0; y < GetSchematicHeight(schematic); y++) {
+                for( int x = 0; x < GetSchematicWidth(schematic);x++) {
+                    if (Char.IsDigit(schematic[y, x])) {
+                        int size = GetDigitSize(schematic, y, x);
+                        if (SymbolIsAdjacent(schematic, y, x, size)) {
+                            Console.WriteLine(GetWholeDigit(schematic, y, x, size));
+                            sum += GetWholeDigit(schematic, y, x, size);
+                            x += size;
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        private static int GetWholeDigit(char [,] schematic, int y, int x, int size) {
+            string sum = "";
+            for(int pointer = x; pointer < x+size; pointer++) {
+                sum += schematic[y, pointer];
+            }
+            return int.Parse(sum);
+        }
+
+        public static int GetDigitSize(char[,] schematic, int y, int x) {
+            for(int pointer = x; pointer < GetSchematicWidth(schematic);) {
+                pointer++;
+                if (pointer == GetSchematicWidth(schematic) || !Char.IsDigit(schematic[y, pointer])) return pointer - x;
+            }
+            return 0;
+        }
+
+        private static bool SymbolIsAdjacent(char[,] schematic, int y, int x, int size) {
+            for(int pointer = x-1; pointer <= x+size; pointer++) {
+                if (pointer >= 0 && pointer < GetSchematicWidth(schematic)) { //Check if pointer is in bounds
+
+                    if (pointer == x - 1 || pointer == x + size)
+                        if (CheckMiddle(schematic,y,pointer)) return true;
+                    if(CheckBottom(schematic, y, pointer) || CheckTop(schematic,y, pointer)) return true;
+                }
+                
+            }
+
+            return false;
+        }
+
+        private static bool CheckTop(char[,] schematic, int y, int pointer) {
+            if (y - 1 < 0) return false;
+            return (schematic[y-1, pointer] != '.' && !Char.IsDigit(schematic[y-1, pointer]));
+        }
+
+        private static bool CheckBottom(char[,] schematic, int y, int pointer) {
+            if (y+1 >= GetSchematicHeight(schematic) ) return false;
+            return (schematic[y + 1, pointer] != '.' && !Char.IsDigit(schematic[y + 1, pointer]));
+        }
+
+        private static bool CheckMiddle(char[,] schematic, int y, int pointer) {
+            return (schematic[y, pointer] != '.' && !Char.IsDigit(schematic[y, pointer]));
+        }
+
+        public static char[ , ] ParseInputSchematic(string data) {
+            string[] data_arr = data.Split("\n");
+            char[,] res = new char[data_arr.Length, data_arr[0].Length]; // new char[height, width]
+
+            for(int y = 0; y < data_arr.Length; y++) {
+                for(int x = 0;  x < data_arr[y].Length; x++) {
+                    res[y,x] = data_arr[y][x];
+                }
+            }
+
+            return res;
+        }
+
+        //So I don't have to remember how to get width and height from 2d arr.
+        public static int GetSchematicWidth(char[,] schematic) {
+            return schematic.GetLength(1);
+        }
+
+        public static int GetSchematicHeight(char[,] schematic) {
+            return schematic.GetLength(0);
+        }
+    }
+}
---
+++ AOC_2023/Challenges_test/Day3/GearRatiosTest.cs
@@ -0,0 +1,291 @@
+﻿using Challenges.Day3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges_test.Day3 {
+    public class GearRatiosTest {
+
+        //-----------------Load the schematic tests------------------------
+        [Test]
+        public void ParseInputMapFourByFour() {
+            string data = "1...\n*...\n.2..\n...3";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            Assert.AreEqual(4, schematic.GetLength(0)); //GetLength(0) == Height
+            Assert.AreEqual(4, schematic.GetLength(1)); //GetLength(0) == Width
+        }
+
+        [Test]
+        public void ParseInputMapOneByOne() {
+            string data = ".";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            Assert.AreEqual(1, schematic.GetLength(0)); //GetLength(0) == Height
+            Assert.AreEqual(1, schematic.GetLength(1)); //GetLength(0) == Width
+        }
+
+        [Test]
+        public void ParseInputMapTwoByTwo() {
+            string data = "..\n..";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            Assert.AreEqual(2, schematic.GetLength(0)); //GetLength(0) == Height
+            Assert.AreEqual(2, schematic.GetLength(1)); //GetLength(0) == Width
+        }
+
+        [Test]
+        public void ParseInputMapThreeByOne() {
+            string data = ".\n.\n.";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            Assert.AreEqual(3, schematic.GetLength(0)); //GetLength(0) == Height
+            Assert.AreEqual(1, schematic.GetLength(1)); //GetLength(0) == Width
+        }
+        //-----------------Finding numbers tests------------------------
+
+        [Test]
+        public void FindSumOfSchematicNumbersNoNumberFound() {
+            string data = "....\n" +
+                          ".1..\n" +
+                          "....\n" +
+                          "....";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.FindSumOfSchematicNumbers(schematic);
+
+            Assert.AreEqual(0, res);
+        }
+
+        [Test]
+        public void FindSumOfSchematicNumbersSymbolLeftOfSingleDigit() {
+            string data = "....\n" +
+                          "*1..\n" +
+                          "....\n" +
+                          "....";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.FindSumOfSchematicNumbers(schematic);
+
+            Assert.AreEqual(1, res);
+        }
+
+        [Test]
+        public void FindSumOfSchematicNumbersSymbolRightOfSingleDigit() {
+            string data = "....\n" +
+                          ".2*.\n" +
+                          "....\n" +
+                          "....";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.FindSumOfSchematicNumbers(schematic);
+
+            Assert.AreEqual(2, res);
+        }
+
+        [Test]
+        public void FindSumOfSchematicNumbersSymbolBottomOfSingleDigit() {
+            string data = "....\n" +
+                          ".3..\n" +
+                          ".*..\n" +
+                          "....";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.FindSumOfSchematicNumbers(schematic);
+
+            Assert.AreEqual(3, res);
+        }
+
+        [Test]
+        public void FindSumOfSchematicNumbersSymbolTopLeftOfSingleDigit() {
+            string data = "*...\n" +
+                          ".4..\n" +
+                          "....\n" +
+                          "....";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.FindSumOfSchematicNumbers(schematic);
+
+            Assert.AreEqual(4, res);
+        }
+
+        [Test]
+        public void FindSumOfSchematicNumbersSymbolTopRightOfSingleDigit() {
+            string data = "..*.\n" +
+                          ".5..\n" +
+                          "....\n" +
+                          "..1.";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.FindSumOfSchematicNumbers(schematic);
+
+            Assert.AreEqual(5, res);
+        }
+
+        [Test]
+        public void FindSumOfSchematicNumbersSymbolBottomLeftOfSingleDigit() {
+            string data = "....\n" +
+                          ".6..\n" +
+                          "*...\n" +
+                          "....";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.FindSumOfSchematicNumbers(schematic);
+
+            Assert.AreEqual(6, res);
+        }
+
+        [Test]
+        public void FindSumOfSchematicNumbersSymbolBottomRightOfSingleDigit() {
+            string data = "....\n" +
+                          ".7..\n" +
+                          "..*.\n" +
+                          "....";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.FindSumOfSchematicNumbers(schematic);
+
+            Assert.AreEqual(7, res);
+        }
+
+        [Test]
+        public void GetDigitSizeOfOne() {
+            string data = "....\n" +
+                          ".7..\n" +
+                          "..*.\n" +
+                          "....";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.GetDigitSize(schematic, 1, 1);
+
+            Assert.AreEqual(1, res);
+        }
+
+        [Test]
+        public void GetDigitSizeOfTwo() {
+            string data = "....\n" +
+                          ".77.\n" +
+                          "..*.\n" +
+                          "....";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.GetDigitSize(schematic, 1, 1);
+
+            Assert.AreEqual(2, res);
+        }
+
+        [Test]
+        public void GetDigitSizeOfThree() {
+            string data = "....\n" +
+                          ".777\n" +
+                          "..*.\n" +
+                          "....";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.GetDigitSize(schematic, 1, 1);
+
+            Assert.AreEqual(3, res);
+        }
+
+        [Test]
+        public void FindSumOfSchematicNumbersThirtyTwo() {
+            string data = "....\n" +
+                          ".32.\n" +
+                          "..*.\n" +
+                          "....";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.FindSumOfSchematicNumbers(schematic);
+
+            Assert.AreEqual(32, res);
+        }
+
+        [Test]
+        public void FindSumOfSchematicNumbersThreehundredAndTwentyOne() {
+            string data = "....\n" +
+                          ".321\n" +
+                          "..*.\n" +
+                          "....";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.FindSumOfSchematicNumbers(schematic);
+
+            Assert.AreEqual(321, res);
+        }
+
+        [Test]
+        public void FindSumOfSchematicNumbersFourhundred() {
+            string data = "....\n" +
+                          ".321\n" +
+                          "..*.\n" +
+                          ".79.";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.FindSumOfSchematicNumbers(schematic);
+
+            Assert.AreEqual(400, res);
+        }
+
+        [Test]
+        public void FindSumOfSchematicNumbersTestData() {
+            string data = "467..114..\n" +
+                          "...*......\n" +
+                          "..35..633.\n" +
+                          "......#...\n" +
+                          "617*......\n" +
+                          ".....+.58.\n" +
+                          "..592.....\n" +
+                          "......755.\n" +
+                          "...$.*....\n" +
+                          ".664.598..";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.FindSumOfSchematicNumbers(schematic);
+
+            Assert.AreEqual(4361, res);
+        }
+
+        [Test]
+        public void PartOneRun() {
+            string data = File.ReadAllText("Day3/input.txt");
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.FindSumOfSchematicNumbers(schematic);
+            Console.WriteLine(res);
+            Assert.AreEqual(527494, res);
+        }
+
+        [Test]
+        public void FromInternet() {
+            string data = "97..\n...*\n100.";
+
+            char[,] schematic = GearRatios.ParseInputSchematic(data);
+
+            int res = GearRatios.FindSumOfSchematicNumbers(schematic);
+            Assert.AreEqual(100, res);
+        }
+
+    }
+}
