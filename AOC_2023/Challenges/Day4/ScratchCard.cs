088ef1914345ac9bff761cd949fdae776d25f13c
Oscar Bäck <oscar.back96@outlook.com>
2023-12-04 13:43:04 +0100

    Day 4 Complete

---
+++ AOC_2023/Challenges/Day4/ScratchCard.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges.Day4 {
+    public class ScratchCard {
+
+        public static int GetPoints(string scratch_data) {
+            int sum = 0;
+
+            string[] scratch_data_arr = scratch_data.Split("\n");
+
+            foreach (string s in scratch_data_arr) {
+                if (s != "") {
+                    string[] scratch_card_arr = s.Split(": ")[1].Split(" | ");
+                    sum += GetScratchCardPoints(scratch_card_arr[0], scratch_card_arr[1]);
+                }
+            }
+
+            return sum;
+        }
+        //2^(n-1)
+        public static int GetScratchCardPoints(string win_nums, string your_nums) {
+            int sum = 0;
+            string[] win_nums_arr = win_nums.Split(" ", StringSplitOptions.RemoveEmptyEntries), your_nums_arr = your_nums.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string win in win_nums_arr) {
+                foreach (string your in your_nums_arr) {
+                    if (win == your) sum++;
+                }
+            }
+
+            return (int)(Math.Pow(2, sum - 1));
+        }
+
+        public static int GetScratchCards(string win_nums, string your_nums) {
+            int sum = 0;
+            string[] win_nums_arr = win_nums.Split(" ", StringSplitOptions.RemoveEmptyEntries), your_nums_arr = your_nums.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string win in win_nums_arr) {
+                foreach (string your in your_nums_arr) {
+                    if (win == your) sum++;
+                }
+            }
+
+            return sum;
+        }
+
+        public static int TotalScratchCards(string scratch_data) {
+            int sum = 0;
+            string[] scratch_data_arr = scratch_data.Split("\n");
+            List<Card> scratch_cards = new List<Card>();
+            foreach (string s in scratch_data_arr) {
+                if (s != "") {
+                    string[] scratch_card_arr = s.Split(": ")[1].Split(" | ");
+                    scratch_cards.Add(new Card() { Win = scratch_card_arr[0], Your = scratch_card_arr[1] });
+                }
+            }
+
+            for (int i = 0; i < scratch_cards.Count; i++) {
+                int points = GetScratchCards(scratch_cards[i].Win, scratch_cards[i].Your);
+                for (int j = i+1; j <= points + i; j++) {
+                    scratch_cards[j].Instances += scratch_cards[i].Instances;
+                }
+                sum += scratch_cards[i].Instances;
+            }
+
+            return sum;
+        }
+    }
+
+    public class Card {
+        public int Instances { get; set; }
+
+        public Card() {
+            Instances = 1;
+        }
+
+        public string Win { get; set; }
+        public string Your { get; set; }
+
+    }
+}
---
+++ AOC_2023/Challenges_test/Day4/ScratchCardTest.cs
@@ -0,0 +1,93 @@
+﻿using Challenges.Day3;
+using Challenges.Day4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges_test.Day4 {
+    public class ScratchCardTest {
+
+        [Test]
+        public void ScratchCardWithZeroPoints() {
+            string data = "Card 1: 1 2 3 4 5 | 6 7 8 9 10";
+
+            int res = ScratchCard.GetPoints(data);
+
+            Assert.AreEqual(0, res);
+        }
+
+        [Test]
+        public void ScratchCardWithOnePoints() {
+            string data = "Card 1: 1 2 3 4 5 | 5 6 7 8 9 10";
+
+            int res = ScratchCard.GetPoints(data);
+
+            Assert.AreEqual(1, res);
+        }
+
+        [Test]
+        public void ScratchCardWithTwoPoints() {
+            string data = "Card 1: 1 2 3 4 5 6 | 5 6 7 8 9 10";
+
+            int res = ScratchCard.GetPoints(data);
+
+            Assert.AreEqual(2, res);
+        }
+
+        [Test]
+        public void ScratchCardWithFourPoints() {
+            string data = "Card 1: 1 2 3 4 5 6 | 4 5 6 7 8 9 10";
+
+            int res = ScratchCard.GetPoints(data);
+
+            Assert.AreEqual(4, res);
+        }
+
+        [Test]
+        public void ScratchCardWithEightPoints() {
+            string data = "Card 1: 1 2 3 4 5 6 | 3 4 5 6 7 8 9 10";
+
+            int res = ScratchCard.GetPoints(data);
+
+            Assert.AreEqual(8, res);
+        }
+
+        [Test]
+        public void Part1TestData() {
+            string data = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+            int res = ScratchCard.GetPoints(data);
+
+            Assert.AreEqual(13, res);
+        }
+
+        [Test]
+        public void Part1() {
+            string data = File.ReadAllText("Day4/input.txt");
+
+            int res = ScratchCard.GetPoints(data);
+
+            Assert.AreEqual(20407, res);
+        }
+
+        [Test]
+        public void Part2TestData() {
+            string data = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+            int res = ScratchCard.TotalScratchCards(data);
+
+            Assert.AreEqual(30, res);
+        }
+
+        [Test]
+        public void Part2() {
+            string data = File.ReadAllText("Day4/input.txt");
+
+            int res = ScratchCard.TotalScratchCards(data);
+
+            Console.WriteLine(res);
+        }
+    }
+}
