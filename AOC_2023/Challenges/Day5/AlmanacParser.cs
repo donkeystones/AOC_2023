3777d56c859b5a09bb50bd29ec921b18555d4309
Oscar Bäck <oscar.back96@outlook.com>
2023-12-05 16:04:57 +0100

    Day 5 Part 1 done

---
+++ AOC_2023/Challenges/Day5/AlmanacParser.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges.Day5 {
+    public class AlmanacParser {
+        public static BigInteger GetLowestPart1(string data) {
+            BigInteger loc = -1;
+
+            string[] data_arr = data.Split("\n");
+
+            BigInteger[] seeds = data_arr[0].Split(": ")[1].Split(" ")
+                                        .Where(x => int.TryParse(x, out _))
+                                        .Select(BigInteger.Parse)
+                                        .ToArray();
+
+            RangeType[] ranges = GetRanges(data_arr);
+
+            foreach (BigInteger seed in seeds) {
+                BigInteger new_loc = GetLoc(seed, ranges);
+                if (loc == -1) loc = new_loc;
+                else if (loc > new_loc) loc = new_loc;
+            }
+
+            return loc;
+        }
+        //(Destination - Source) + seed = soil location
+        private static BigInteger GetLoc(BigInteger seed, RangeType[] ranges) {
+            Console.WriteLine("Seed: " + seed);
+            for(int range = 0; range < ranges.Length; range++) {
+                foreach(Map map in ranges[range].maps) {
+                    if(seed >= map.source_id && seed < map.source_id + map.range_length) { //If sourcerange is within current seed
+                        seed = (map.destination_id - map.source_id) + seed;
+                        break;
+                    }
+                }
+                Console.WriteLine("Seed after " + range + " map: " + seed);
+
+            }
+            return seed;
+        }
+
+        public static RangeType[] GetRanges(string[] data_arr) {
+            List<RangeType> ranges = new List<RangeType>();
+            int ranges_pointer = 0;
+            for(int i = 2; i < data_arr.Length; i++) {
+                if (data_arr[i] == "seed-to-soil map:" ||
+                    data_arr[i] == "soil-to-fertilizer map:" ||
+                    data_arr[i] == "fertilizer-to-water map:" ||
+                    data_arr[i] == "water-to-light map:" ||
+                    data_arr[i] == "light-to-temperature map:" ||
+                    data_arr[i] == "temperature-to-humidity map:" ||
+                    data_arr[i] == "humidity-to-location map:") {
+                    ranges.Add(new RangeType());
+                    int j;
+                    for(j = i+1; j < data_arr.Length; j++) {
+                        if (data_arr[j] != "")
+                            ranges[ranges_pointer].maps.Add(GetMap(data_arr[j]));
+                        else break;
+                    }
+                    ranges_pointer++;
+                    i = j;
+                }
+            }
+
+            return ranges.ToArray();
+        }
+        //Destination Source Range_Length
+        //50          98     2
+         
+        public static Map GetMap(string map) {
+            string[] map_arr = map.Split(" ");
+            return new Map() {
+                source_id = BigInteger.Parse(map_arr[1]),
+                destination_id = BigInteger.Parse(map_arr[0]),
+                range_length = BigInteger.Parse((map_arr[2]))
+            };
+        }
+
+    }
+
+    public class RangeType {
+        public List<Map> maps;
+        public RangeType() { maps = new List<Map>(); }
+    }
+
+    public class Map {
+        public BigInteger source_id;
+        public BigInteger destination_id;
+        public BigInteger range_length;
+    }
+
+}
---
+++ AOC_2023/Challenges_test/Day5/AlmanacParserTest.cs
@@ -0,0 +1,29 @@
+﻿using Challenges.Day5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges_test.Day5 {
+    public class AlmanacParserTest {
+        [Test]
+        public void Part1TestData() {
+            string data = File.ReadAllText("Day5/testdata.txt");
+
+            BigInteger res = AlmanacParser.GetLowestPart1(data);
+
+            Assert.IsTrue(new BigInteger(35).Equals(res));
+        }
+
+        [Test] public void Part1() {
+            string data = File.ReadAllText("Day5/input.txt");
+
+            BigInteger res = AlmanacParser.GetLowestPart1(data);
+
+            Console.WriteLine(res.ToString());
+            Assert.IsTrue(new BigInteger(910845529).Equals(res));
+        }
+    }
+}
